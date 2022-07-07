9e05f9c6fbcab1fd11558376ecd062a1bd010e4f
Abbbyccc <abbychien@hotmail.com>
2022-07-07 12:06:09 +0100

    add suggetion dropdown list

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
 import data from "./data.js"
 
-const acronym_input = document.getElementById('acronym_input').value
 const sumbmit_btn = document.getElementById('search_button')
+const acronym_input = document.getElementById('acronym_input')
 
-let onChange = (event) => {
-    console.log(this.event.data)
+
+sumbmit_btn.addEventListener('click', (event) => {
+    event.preventDefault()
+    const acronym_input = document.getElementById('acronym_input')
+    let fullNameOutput = document.getElementById('fullName')
+    fullNameOutput.innerHTML = `Sorry ${acronym_input.value} doesn't exist`
+    for (let i = 0; i < data.length; i++) {
+        if (acronym_input.value.toUpperCase() == data[i]['acronym']) {
+            fullNameOutput.innerHTML = data[i]['fullName']
+        }
+    }
+})
+
+function dropDownList() {
+
+    const acronym_input = document.getElementById('acronym_input')
+    const dropdown_list = document.getElementById('dropdown')
+
+    acronym_input.addEventListener("keyup", (event) => {
+        removeAllChildNodes(dropdown_list)
+        const searchName = event.target.value.toUpperCase()
+        // console.log(data['acronym'])
+        for (let i = 0; i < data.length; i++) {
+            if (data[i]['acronym'].toUpperCase().includes(searchName)) {
+                const option_value = document.createElement("option")
+                option_value.innerHTML = data[i]['acronym']
+                dropdown_list.appendChild(option_value)
+            }
+        }
+    })
 }
 
-acronym_input.addEventListener('input', onChange)
 
-const loopData = () => {
 
+// CLEAR THE CONTENT BEFORE SEARCH AGAIN
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
 }
-
+dropDownList()
 
 // const updateDebounceValue = () => {
 //     debounceValue.innerHTML = acronym_input.value;
