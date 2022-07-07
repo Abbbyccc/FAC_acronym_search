6a417687c3dbf92bd0d855919de5db4b18454382
Abbbyccc <abbychien@hotmail.com>
2022-07-07 16:35:36 +0100

    Partial matching

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 import data from "./data.js"
 
 const sumbmit_btn = document.getElementById('search_button')
+
+function clearOutput() {
+
+}
 sumbmit_btn.addEventListener('click', (event) => {
     event.preventDefault()
     const acronym_input = document.getElementById('acronym_input')
     const fullNameOutput = document.getElementById('fullName')
-    fullNameOutput.innerHTML = `Sorry ${acronym_input.value} doesn't exist`
+    const error_msg = document.getElementById("error_msg")
+    error_msg.innerHTML = ""
+    console.log(error_msg.innerHTML)
+    removeAllChildNodes(fullNameOutput)
+
     for (let i = 0; i < data.length; i++) {
-        if (acronym_input.value.toUpperCase() == data[i]['acronym'].toUpperCase()) {
-            fullNameOutput.innerHTML = data[i]['fullName']
+        if (data[i]['acronym'].toUpperCase().includes(acronym_input.value.toUpperCase()) && acronym_input.value.toUpperCase() !== "") {
+            let result_value = document.createElement("li")
+            result_value.innerHTML = data[i]['fullName']
+            fullNameOutput.appendChild(result_value)
+
+        } else {
+            error_msg.innerHTML = `Sorry ${acronym_input.value} doesn't exist`
         }
     }
 })
+
+
+
 
 const acronym_input = document.getElementById('acronym_input')
 const dropdown_list = document.getElementById('dropdown')
@@ -28,7 +44,7 @@ acronym_input.addEventListener("keydown", (event) => {
 
     }
     for (let i = 0; i < filteredChar.length; i++) {
-        const option_value = document.createElement("option")
+        let option_value = document.createElement("option")
         option_value.value = filteredChar[i]
         dropdown_list.appendChild(option_value)
     }
